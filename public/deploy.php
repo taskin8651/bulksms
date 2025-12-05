e6f5d44d41b3cb85fc259332eba91d5bf661791f
taskin8651 <mdsayebalam10@gmail.com>
2025-12-05 18:07:42 +0530

    ddgf

--- public/deploy.php
+++ public/deploy.php
@@ -5,7 +5,7 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
 $secret = 'mysecret'; // optional
 
 // Go to your project folder
-$projectRoot = '/home/smartuni/businessbarhega.in';
+$projectRoot = '/home/smartuni/public_html/businessbarhega.in';
 
 chdir($projectRoot);
 
